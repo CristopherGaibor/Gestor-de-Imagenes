1fcda92260928e3f81bd11e422cbbe636170e0aa
Piwody <mateosteve1312@gmail.com>
2026-05-31 22:54:35 -0500

    feature: equipacion de midelware por seguridad de peticiones

---
+++ image-service/internal/middleware/auth.go
@@ -0,0 +1,39 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func RequireAuth() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Acceso denegado: Token no proporcionado"})
+			c.Abort()
+			return
+		}
+
+		req, err := http.NewRequest("GET", "http://auth-service:8000/api/auth/verify", nil)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error interno al preparar la verificación"})
+			c.Abort()
+			return
+		}
+
+		req.Header.Add("Authorization", authHeader)
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+
+		if err != nil || resp.StatusCode != http.StatusOK {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Acceso denegado: Token inválido o expirado"})
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
+}
